d8f22d84fdfe9103539b805910e47561000fa7e8
atofarides <antonist10@gmail.com>
2019-05-14 18:11:24 +0200

    Clean data and change extraction method and
    Tended to erroneous links in 2018 data and changed web scraping method from getHTMLLinks to html_attrs

--- golf.r
+++ golf.r
@@ -1,5 +1,6 @@
 #Loading libraries
 library("XML")
+library("rvest")
 library("tidyverse")
 library("stringr")
 library("ggplot2")
@@ -14,41 +15,30 @@ download.file('http://www.lpga.com/statistics/money/official-money?year=2018', d
 colTypes <- c("numeric","character","character","numeric")
 players <- readHTMLTable("golf.html", colClasses = colTypes, stringsAsFactors = FALSE, as.data.frame = TRUE, which = 1)
 str(players)
-class(players)
 
 # Converting official money to numeric
 players$`Official Money`<- as.numeric(gsub("\\$|,","",players$`Official Money`))
 str(players)
 
+# Identifying errors in player profile links and fixing 
+cleanHTML <- sapply(read_lines("golf.html"),str_replace, "(/players/.*) ","\\1-")
+write(cleanHTML, "golfClean.html")
+
 # Extracting player ID
-playerProfileURLPath <- getHTMLLinks("golf.html", xpQuery = "//a[contains(@href,'players') and contains(@href,'overview')]/@href")
-playerProfile <- data.frame(matrix(unlist(strsplit(playerProfileURLPath,"/")),nrow=length(playerProfileURLPath), byrow = TRUE),stringsAsFactors = FALSE) [,3:4]
-names(playerProfile) <- c("Name","ID")
+golfHTML <- read_html("golfClean.html")
+golfPlayers <- html_nodes(golfHTML, xpath = "//tbody/tr/td[contains(@class, 'table-content left')]/a")
+golfPlayersURL <- unlist(html_attrs(golfPlayers))
+playersID <- data.frame(matrix(unlist(strsplit(golfPlayersURL,"/")),nrow=length(golfPlayersURL), byrow = TRUE),stringsAsFactors = FALSE) [,4]
 
-if(dim(playerProfile)[1] == dim(players)[1]){
-  print("Numbers match")
-} else {
-  print(paste("Number of players found is ", dim(players)[1]," while number of URLs is ",dim(playerProfile)[1]))
-}
-
-# Add a player row manually. Row was originally ommitted due to bad link on the website
-playerProfile <- add_row(playerProfile,Name = c("wei-ling-hsu"),ID =c("98290"), .after=30)
-
-players <- add_column(players,as.numeric(playerProfile[,2]), .after = 1)
+players <- add_column(players,as.numeric(playersID), .after = 1)
 names(players)[2] <- c("ID")
 
 str(players)
 
 # Extracting Player Country
-playerCountryImageURLPath <- getHTMLExternalFiles("golf.html", xpQuery = "//img[contains(@src,'countries')]/@src")
-
-playerCountries <- toupper(str_match(playerCountryImageURLPath,"countries/(.*[a-z])\\.")[,2])
-
-if(length(playerCountries) == dim(players)[1]){
-  print("Numbers match")
-} else {
-  print(paste("Number of players found is ", dim(players)[1]," while number of nationalities is ",length(playerCountries)))
-}
+golfPlayersCountries <- html_nodes(golfHTML, xpath = "//tbody/tr/td[contains(@class, 'table-content left')]/a/div/img")
+golfPlayersCountriesURL <- unlist(html_attrs(golfPlayersCountries))
+playerCountries <- toupper(str_match(golfPlayersCountriesURL,"countries/(.*[a-z])\\.")[,2])
 
 players <- add_column(players,as.factor(playerCountries), .after = "Name")
 names(players)[4] <- c("Country")
@@ -56,10 +46,10 @@ names(players)[4] <- c("Country")
 str(players)
 
 # Finding if there is a corellation between money and events played
-ggplot(data = players, mapping = aes(x=players$`Events Played`, y=players$`Official Money`)) +
-  geom_point() +
-  scale_y_log10()
+# ggplot(data = players, mapping = aes(x=players$`Events Played`, y=players$`Official Money`)) +
+#  geom_point() +
+#  scale_y_log10()
 
-cor(players$`Events Played`,players$`Official Money`)
+# cor(players$`Events Played`,players$`Official Money`)
 
 # seems like there is a positive corellation between events played and money made

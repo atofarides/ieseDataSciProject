6b6361cd64ab1271e694917a868b2ce84f48d442
atofarides <antonist10@gmail.com>
2019-05-16 17:16:06 +0200

    Update golf.r
    Updating comments

--- golf.r
+++ golf.r
@@ -5,7 +5,7 @@ library("tidyverse")
 library("stringr")
 library("ggplot2")
 
-# Initiate variable
+# Initiate global variables
 playersAllYears <- data.frame(
   Year = numeric(),
   Rank = numeric(),
@@ -17,7 +17,6 @@ playersAllYears <- data.frame(
 )
 
 year <- seq(from = 2018, to = 2000, by = -1)
-#y <- c("2018")
 url <- c('http://www.lpga.com/statistics/money/official-money?year=')
 colTypes <- c("numeric","character","character","numeric")
 
@@ -27,14 +26,14 @@ for (y in year) {
   # Defining working URL 
   tableURL <- paste(url,y,sep = "")
   
-  # Players scraped in table 
+  # Players' Table Text scraped
   players <- readHTMLTable(tableURL, colClasses = colTypes, stringsAsFactors = FALSE, as.data.frame = TRUE, which = 1)
   names(players)[3:4] <- c("OfficialMoney", "EventsPlayed")
   
-  # Converting official money to numeric
+  # Converting official money to numeric to enable analysis
   players$`OfficialMoney`<- as.numeric(gsub("\\$|,","",players$`OfficialMoney`))
   
-  # Identifying errors in player profile links, fixing, and writing in new file
+  # Identifying erroneous spacing in player profile links, fixing
   if (str_detect(read_html(tableURL),"/players/.+ [a-z0-9/-]*>")){
     print(paste("Errors identified in table of year", y,"... Fixing!"))
     cleanHTML <- paste(sapply(readLines(tableURL),str_replace, "(/players/.+) ([a-z0-9/-]*>)","\\1-\\2"),collapse="")
@@ -45,6 +44,7 @@ for (y in year) {
     golfHTML <- read_html(tableURL)
   }
   
+  # Reading profile links 
   golfPlayers <- html_nodes(golfHTML, xpath = "//tbody/tr/td[contains(@class, 'table-content left')]/a")
   
   # Identifying missing link attributes
@@ -83,10 +83,10 @@ for (y in year) {
   names(players)[4] <- c("Country")
   
   # Add year to main table
-  
   players <- add_column(players, y, .before = "Rank")
   names(players)[1] <- c("Year")
   
+  # Add this years' players to main table
   playersAllYears <- rbind(playersAllYears, players)
 }
 str(playersAllYears)
